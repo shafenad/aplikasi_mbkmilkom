36ce2ccf27174329cb173a5a058d15758a25de74
Risma Dwi A <anggrainirismadwi@gmail.com>
2023-11-15 19:13:47 +0700

    upload rinaa

--- forms/Yusrina/Bismillah duarr/Bismillah duarr/konversinilai.Designer.cs
+++ forms/Yusrina/Bismillah duarr/Bismillah duarr/konversinilai.Designer.cs
@@ -33,6 +33,7 @@
             this.dashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mitraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusMitraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusMitraToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.mataKuliahToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.konversiSKSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.konversiNilaiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -52,7 +53,6 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.statusMitraToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -62,6 +62,7 @@
             // menuStrip1
             // 
             this.menuStrip1.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.dashboardToolStripMenuItem,
@@ -72,7 +73,8 @@
             this.profilToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1067, 34);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1200, 39);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -80,7 +82,7 @@
             // 
             this.dashboardToolStripMenuItem.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dashboardToolStripMenuItem.Name = "dashboardToolStripMenuItem";
-            this.dashboardToolStripMenuItem.Size = new System.Drawing.Size(111, 30);
+            this.dashboardToolStripMenuItem.Size = new System.Drawing.Size(131, 35);
             this.dashboardToolStripMenuItem.Text = "Dashboard";
             // 
             // mitraToolStripMenuItem
@@ -90,36 +92,42 @@
             this.statusMitraToolStripMenuItem1});
             this.mitraToolStripMenuItem.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mitraToolStripMenuItem.Name = "mitraToolStripMenuItem";
-            this.mitraToolStripMenuItem.Size = new System.Drawing.Size(64, 30);
+            this.mitraToolStripMenuItem.Size = new System.Drawing.Size(76, 35);
             this.mitraToolStripMenuItem.Text = "Mitra";
             this.mitraToolStripMenuItem.Click += new System.EventHandler(this.mitraToolStripMenuItem_Click);
             // 
             // statusMitraToolStripMenuItem
             // 
             this.statusMitraToolStripMenuItem.Name = "statusMitraToolStripMenuItem";
-            this.statusMitraToolStripMenuItem.Size = new System.Drawing.Size(224, 30);
+            this.statusMitraToolStripMenuItem.Size = new System.Drawing.Size(266, 40);
             this.statusMitraToolStripMenuItem.Text = "Pengajuan Mitra";
             this.statusMitraToolStripMenuItem.Click += new System.EventHandler(this.statusMitraToolStripMenuItem_Click);
+            // 
+            // statusMitraToolStripMenuItem1
+            // 
+            this.statusMitraToolStripMenuItem1.Name = "statusMitraToolStripMenuItem1";
+            this.statusMitraToolStripMenuItem1.Size = new System.Drawing.Size(266, 40);
+            this.statusMitraToolStripMenuItem1.Text = "Status Mitra";
             // 
             // mataKuliahToolStripMenuItem
             // 
             this.mataKuliahToolStripMenuItem.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mataKuliahToolStripMenuItem.Name = "mataKuliahToolStripMenuItem";
-            this.mataKuliahToolStripMenuItem.Size = new System.Drawing.Size(115, 30);
+            this.mataKuliahToolStripMenuItem.Size = new System.Drawing.Size(137, 35);
             this.mataKuliahToolStripMenuItem.Text = "Mata Kuliah";
             // 
             // konversiSKSToolStripMenuItem
             // 
             this.konversiSKSToolStripMenuItem.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.konversiSKSToolStripMenuItem.Name = "konversiSKSToolStripMenuItem";
-            this.konversiSKSToolStripMenuItem.Size = new System.Drawing.Size(121, 30);
+            this.konversiSKSToolStripMenuItem.Size = new System.Drawing.Size(144, 35);
             this.konversiSKSToolStripMenuItem.Text = "Konversi SKS";
             // 
             // konversiNilaiToolStripMenuItem
             // 
             this.konversiNilaiToolStripMenuItem.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.konversiNilaiToolStripMenuItem.Name = "konversiNilaiToolStripMenuItem";
-            this.konversiNilaiToolStripMenuItem.Size = new System.Drawing.Size(127, 30);
+            this.konversiNilaiToolStripMenuItem.Size = new System.Drawing.Size(148, 35);
             this.konversiNilaiToolStripMenuItem.Text = "Konversi Nilai";
             // 
             // profilToolStripMenuItem
@@ -129,21 +137,21 @@
             this.logOutToolStripMenuItem});
             this.profilToolStripMenuItem.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.profilToolStripMenuItem.Name = "profilToolStripMenuItem";
-            this.profilToolStripMenuItem.Size = new System.Drawing.Size(64, 30);
+            this.profilToolStripMenuItem.Size = new System.Drawing.Size(73, 35);
             this.profilToolStripMenuItem.Text = "Profil";
             this.profilToolStripMenuItem.Click += new System.EventHandler(this.profilToolStripMenuItem_Click);
             // 
             // informasiAkunToolStripMenuItem
             // 
             this.informasiAkunToolStripMenuItem.Name = "informasiAkunToolStripMenuItem";
-            this.informasiAkunToolStripMenuItem.Size = new System.Drawing.Size(212, 30);
+            this.informasiAkunToolStripMenuItem.Size = new System.Drawing.Size(253, 40);
             this.informasiAkunToolStripMenuItem.Text = "Informasi Akun";
             this.informasiAkunToolStripMenuItem.Click += new System.EventHandler(this.informasiAkunToolStripMenuItem_Click);
             // 
             // logOutToolStripMenuItem
             // 
             this.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
-            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(212, 30);
+            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(253, 40);
             this.logOutToolStripMenuItem.Text = "Log out";
             // 
             // btn_simpannilai
@@ -152,10 +160,10 @@
             this.btn_simpannilai.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.btn_simpannilai.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_simpannilai.ForeColor = System.Drawing.Color.White;
-            this.btn_simpannilai.Location = new System.Drawing.Point(939, 492);
-            this.btn_simpannilai.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_simpannilai.Location = new System.Drawing.Point(1056, 615);
+            this.btn_simpannilai.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_simpannilai.Name = "btn_simpannilai";
-            this.btn_simpannilai.Size = new System.Drawing.Size(100, 33);
+            this.btn_simpannilai.Size = new System.Drawing.Size(112, 41);
             this.btn_simpannilai.TabIndex = 3;
             this.btn_simpannilai.Text = " Simpan";
             this.btn_simpannilai.UseVisualStyleBackColor = false;
@@ -171,13 +179,13 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 1, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 90);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(18, 112);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1027, 362);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1155, 452);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // panel1
@@ -189,10 +197,10 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.cbx_matkulajukan);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(4, 4);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(4, 5);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(505, 354);
+            this.panel1.Size = new System.Drawing.Size(568, 442);
             this.panel1.TabIndex = 0;
             // 
             // tbx_bukti
@@ -201,20 +209,20 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbx_bukti.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_bukti.Location = new System.Drawing.Point(23, 250);
-            this.tbx_bukti.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx_bukti.Location = new System.Drawing.Point(26, 273);
+            this.tbx_bukti.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbx_bukti.Name = "tbx_bukti";
-            this.tbx_bukti.Size = new System.Drawing.Size(460, 30);
+            this.tbx_bukti.Size = new System.Drawing.Size(517, 34);
             this.tbx_bukti.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Poppins SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(19, 218);
+            this.label3.Location = new System.Drawing.Point(19, 231);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 30);
+            this.label3.Size = new System.Drawing.Size(118, 37);
             this.label3.TabIndex = 5;
             this.label3.Text = "Link Bukti";
             // 
@@ -225,10 +233,10 @@
             this.btn_inputmatkul.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.btn_inputmatkul.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_inputmatkul.ForeColor = System.Drawing.Color.White;
-            this.btn_inputmatkul.Location = new System.Drawing.Point(23, 167);
-            this.btn_inputmatkul.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_inputmatkul.Location = new System.Drawing.Point(26, 353);
+            this.btn_inputmatkul.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btn_inputmatkul.Name = "btn_inputmatkul";
-            this.btn_inputmatkul.Size = new System.Drawing.Size(140, 31);
+            this.btn_inputmatkul.Size = new System.Drawing.Size(158, 39);
             this.btn_inputmatkul.TabIndex = 4;
             this.btn_inputmatkul.Text = "Input Matkul";
             this.btn_inputmatkul.UseVisualStyleBackColor = false;
@@ -240,20 +248,20 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbx_nilai.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_nilai.Location = new System.Drawing.Point(23, 135);
-            this.tbx_nilai.Margin = new System.Windows.Forms.Padding(4);
+            this.tbx_nilai.Location = new System.Drawing.Point(26, 169);
+            this.tbx_nilai.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbx_nilai.Name = "tbx_nilai";
-            this.tbx_nilai.Size = new System.Drawing.Size(460, 30);
+            this.tbx_nilai.Size = new System.Drawing.Size(517, 34);
             this.tbx_nilai.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Poppins SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 100);
+            this.label2.Location = new System.Drawing.Point(21, 125);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 30);
+            this.label2.Size = new System.Drawing.Size(64, 37);
             this.label2.TabIndex = 2;
             this.label2.Text = "Nilai";
             // 
@@ -264,20 +272,20 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cbx_matkulajukan.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbx_matkulajukan.FormattingEnabled = true;
-            this.cbx_matkulajukan.Location = new System.Drawing.Point(23, 49);
-            this.cbx_matkulajukan.Margin = new System.Windows.Forms.Padding(4);
+            this.cbx_matkulajukan.Location = new System.Drawing.Point(26, 61);
+            this.cbx_matkulajukan.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbx_matkulajukan.Name = "cbx_matkulajukan";
-            this.cbx_matkulajukan.Size = new System.Drawing.Size(460, 34);
+            this.cbx_matkulajukan.Size = new System.Drawing.Size(517, 39);
             this.cbx_matkulajukan.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Poppins SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(19, 14);
+            this.label1.Location = new System.Drawing.Point(19, 19);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(204, 30);
+            this.label1.Size = new System.Drawing.Size(249, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Matkul yang diajukan";
             // 
@@ -291,11 +299,11 @@
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2});
-            this.dataGridView1.Location = new System.Drawing.Point(517, 4);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Location = new System.Drawing.Point(581, 5);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(506, 354);
+            this.dataGridView1.Size = new System.Drawing.Size(570, 442);
             this.dataGridView1.TabIndex = 1;
             // 
             // Column1
@@ -310,23 +318,17 @@
             this.Column2.MinimumWidth = 6;
             this.Column2.Name = "Column2";
             // 
-            // statusMitraToolStripMenuItem1
-            // 
-            this.statusMitraToolStripMenuItem1.Name = "statusMitraToolStripMenuItem1";
-            this.statusMitraToolStripMenuItem1.Size = new System.Drawing.Size(224, 30);
-            this.statusMitraToolStripMenuItem1.Text = "Status Mitra";
-            // 
             // konversinilai
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.btn_simpannilai);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "konversinilai";
             this.Text = "Baskom";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
